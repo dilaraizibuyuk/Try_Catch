6756f8d105b929eece5caee17fa0b37c9d626b1b
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-04 19:53:57 +0300

    Proje dosyası ekle.

---
+++ Try_Catch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("1. sayıyı giriniz:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("2. sayıyı giriniz:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+
+            try
+            {
+                Console.WriteLine("{0}/{1}={2}",x,y,x/y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata oluştu:{0}",e);
+                
+            }
+            finally
+            {
+                Console.WriteLine("İy günler..");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ornek2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Yaşınızı giriniz:");
+                int yas = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bilgilendirmeniz için teşekkür ederiz.");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+          
+            Console.ReadKey();
+        }
+    }
+}
